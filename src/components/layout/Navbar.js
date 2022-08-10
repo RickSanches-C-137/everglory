a83ade643ad99f3aa2aa75b8e1855fba67654bee
Sanni Abdulfatai <esanni5@gmail.com>
2022-08-10 13:23:41 +0100

    all

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
-import LogOutBtn from "../auth/LogOutBtn";
 
 function Navbar() {
   const [nav, setNav] = useState(true);
-  const { loggedIn } = useContext(AuthContext);
 
   return (
     <div class="bg-gray-900">
@@ -65,36 +62,6 @@ function Navbar() {
                   <span>FAQs</span>
                 </li>
               </Link>
-
-              <li className="py-1">
-                {!loggedIn && (
-                  <>
-                    <Link to="/register">
-                      <span className="md:ml-5 p-3 rounded-md text-gray-200 font-bold text-sm md:ring-2 ring-blue-500/50 md:bg-white md:text-gray-900 hover:text-blue-500 duration-700">
-                        Open account
-                      </span>
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="md:ml-5 p-3 rounded-md text-gray-200 font-bold text-sm md:ring-2 ring-blue-500/50  hover:bg-white hover:text-gray-900 duration-700"
-                    >
-                      Sign in
-                    </Link>
-                  </>
-                )}
-                {loggedIn && (
-                  <>
-                    <Link
-                      to="/dashboard"
-                      className="text-gray-200 font-bold ml-4"
-                    >
-                      Dashboard
-                    </Link>
-
-                    <LogOutBtn />
-                  </>
-                )}
-              </li>
             </ul>
           )}
         </nav>
